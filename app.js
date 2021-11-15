9a0670e3c34b5b3b86a80a3325f1dc9ed790af2d
Van Le <70436051+iRok166@users.noreply.github.com>
2021-11-15 22:15:59 +0700

    ThavaShop 1.1

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const categoryRouter = require('./routes/category');
+const signinRouter = require('./routes/signin');
+const registerRouter = require('./routes/register');
+const detailRouter = require('./routes/detail');
+const basketRouter = require('./routes/basket');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +26,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/category', categoryRouter);
+app.use('/signin', signinRouter);
+app.use('/register', registerRouter);
+app.use('/detail', detailRouter);
+app.use('/basket', basketRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
