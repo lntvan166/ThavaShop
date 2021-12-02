6d9012b429f3c498a0678912bd321e43dba3c392
Van Le <70436051+iRok166@users.noreply.github.com>
2021-12-02 19:11:26 +0700

    men3_3 => men3_2

--- components/products/productService.js
+++ components/products/productService.js
@@ -2,4 +2,8 @@ const Product = require("../../models/Product")
 
 exports.list = () => Product.find({})
 
-exports.productByID = (id) => Product.findOne({_id: id}).lean()
+exports.productByID = (id) => Product.findOne({ _id: id }).lean()
+
+exports.count = () => Product.count({}).exec()
+
+exports.findByPage = (page, itemPerPage) => Product.find({}).skip(page * itemPerPage).limit(itemPerPage)
